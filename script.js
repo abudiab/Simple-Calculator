a557924f4bb92275881086b5d0d95687ba07661d
Mahmoud <mahmoud.w.diab@gmail.com>
2021-03-05 11:09:13 +0200

    fully functional

--- script.js
+++ script.js
@@ -29,19 +29,35 @@ const addDecimal = () => {
   }
 };
 
+// Calculate first and second values depending on operator
+const calculate = {
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "=": (firstNumber, secondNumber) => secondNumber,
+};
+
 const useOperator = (operator) => {
   const currentValue = Number(calculatorDisplay.textContent);
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
   // Assign firstValue if no value
   if (!firstValue) {
     firstValue = currentValue;
   } else {
-    console.log("current value", currentValue);
+    // console.log(firstValue, operatorValue, currentValue);
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    // console.log("calculation", calculation);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
   }
   //   ready for the next value, store operator
   awaitingNextValue = true;
   operatorValue = operator;
-  console.log("firstvalue", firstValue);
-  console.log("operator", operator);
 };
 
 // Add Event Listeners for numbers, operators, decimal buttons
